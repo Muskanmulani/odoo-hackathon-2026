9e1e841fcc0cfb70be8403eead47abcf3c294ca2
Muskanmulani <142891591+Muskanmulani@users.noreply.github.com>
2026-07-12 12:34:20 +0530

    Implement user authentication with JWT

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+
+const protect = require("./middleware/authMiddleware");
 require("dotenv").config();
 
 const express = require("express");
@@ -10,6 +12,9 @@ connectDB();
 
 app.use(cors());
 app.use(express.json());
+const authRoutes = require("./routes/authRoutes");
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
     res.send("AssetFlow Backend Running 🚀");
@@ -19,4 +24,12 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
+});
+app.get("/api/profile", protect, (req, res) => {
+
+    res.json({
+        message: "Protected route accessed",
+        user: req.user
+    });
+
 });
